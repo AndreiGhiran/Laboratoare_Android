c3f947ed8a59107ffee6b93e13b0b61ea4f2173a
AndreiG <48528839+AndreiGhiran@users.noreply.github.com>
2020-04-29 23:23:20 +0300

    Encoding almost done

--- MorseCode/app/src/main/java/com/example/morsecode/MainActivity.java
+++ MorseCode/app/src/main/java/com/example/morsecode/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.morsecode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView morseCodeView;
+    EditText inputText;
+    Button encodeButton;
+    Translator translator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        morseCodeView = (TextView) findViewById(R.id.MorceCodeView);
+        inputText =  (EditText) findViewById(R.id.text_input);
+        encodeButton = (Button) findViewById(R.id.encodeButton);
+        try {
+            translator = new Translator(getAssets().open("MorseCode.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        encodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textToEncode = String.valueOf(inputText.getText());
+                morseCodeView.setText(translator.Encode(textToEncode));
+            }
+        });
     }
 }
---
+++ MorseCode/app/src/main/java/com/example/morsecode/Translator.java
@@ -0,0 +1,62 @@
+package com.example.morsecode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Translator  extends AppCompatActivity {
+    String[] Characters;
+    HashMap<String, String> char_to_Code = new HashMap<String, String>();
+    HashMap<String, String> code_to_Char = new HashMap<String, String>();
+    InputStream in;
+    public Translator(InputStream stream){
+        in = stream;
+        Initialize();
+    }
+
+    void Initialize(){
+        String text = "";
+        try {
+            int size = in.available();
+            byte[]buffer = new byte[size];
+            in.read(buffer);
+            in.close();
+            text = new String(buffer);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        String[] lines =  text.split("\n");
+        String[] line;
+        for(int i = 0;  i< lines.length; i++){
+            line = lines[i].split(" ");
+            code_to_Char.put(line[1].trim(),line[0].trim());
+            char_to_Code.put(line[0].trim(),line[1].trim());
+        }
+    }
+
+    String Encode(String text){
+        String code = "";
+        for(int i = 0; i<text.length();i++)
+        {
+            switch (text.charAt(i)){
+                case ' ':
+                    code += " 7 ";
+                    break;
+                default:
+                    code += char_to_Code.get(String.valueOf(text.charAt(i)).toUpperCase());
+            }
+            code+="3";
+        }
+        return code;
+    }
+
+    String Decode(String code){
+        String text = "";
+        return text;
+    }
+    }
